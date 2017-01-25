5b3f0feeee7cc307aa651ca057d5f43f1f9f5f1b
swapnilaust <iamswapnilbiswas@gmail.com>
2017-01-25 21:36:37 +0600

    floyd warshall

---
+++ Graph_Theory/floyd_warshall.cpp
@@ -0,0 +1,23 @@
+
+typedef long long ll;
+const ll inf = 1e9+20;
+
+void init(int n ){
+    for( int i = 0; i <= n; i++ ){
+        for( int j = 0; j <= n; j++ ){
+            if( i == j ) cost[i][j] = 0;
+            else cost[i][j] = inf;
+        }
+    }
+}
+
+void floydWarshall (int n) {
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cost [i][j] = min(cost [i][j], cost [i][k] + cost [k][j]);
+            }
+        }
+    }
+}
+
